a5ae984ff48568d96183e9025968fc13eba82206
Lucas Alves de Araujo <lucas.al.araujo@hotmail.com>
2023-10-08 18:11:24 -0300

    chore: move transations consult to context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,9 +6,7 @@ import totalImg from '../../assets/total.svg'
 import { TransactionsContext } from '../../TransactionsContext';
 
 export function Summary() {
-    const transactions = useContext(TransactionsContext);
-
-    console.log(transactions);
+    const {transactions} = useContext(TransactionsContext);
 
 
     return (
